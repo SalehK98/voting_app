fa38c3cba74ef54bcce74b9b49dabfb118d6b613
Saleh Kalouti <salehkalouti@gmail.com>
2023-02-18 19:13:18 +0200

    Delete .history directory

--- .history/src/App_20230216234222.js
+++
@@ -1,7 +0,0 @@
-import "./assets/App.css";
-
-function App() {
-  return <div className="App">saleh kalouti</div>;
-}
-
-export default App;
--- .history/src/pages/Login/login_20230218145850.js
+++
@@ -1,8 +0,0 @@
-import users from "../../data/users";
-
-function Login(username, password) {
-  //   const isUser = false;
-  users.forEach((el) => {
-    return el.name === username && el.password === password;
-  });
-}
